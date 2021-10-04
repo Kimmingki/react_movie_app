dc6da06aae63a6a429c19000f90beea2d5b099f9
Minki <rlaalsrl9569@naver.com>
2021-10-05 01:09:25 +0900

    Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
+// state를 사용하기 위하여 class component를 사용
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -12,7 +14,9 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
     this.setState({ movies, isLoading: false });
   };
 
@@ -21,8 +25,24 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : // object를 풀어줄 때 map 함수를 사용, jsx에서 props를 통해 값 전달, key는 표현되지 않지만 필수 props
+            movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
