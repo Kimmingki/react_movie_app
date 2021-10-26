5fb3ec72c185e3c6c6055741dfd80774e86ac5b4
Minki <rlaalsrl9569@naver.com>
2021-10-26 22:49:15 +0900

    making the movie app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 // state를 사용하기 위하여 class component를 사용
 class App extends React.Component {
@@ -27,21 +28,30 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : // object를 풀어줄 때 map 함수를 사용, jsx에서 props를 통해 값 전달, key는 표현되지 않지만 필수 props
-            movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            ))}
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {
+              // object를 풀어줄 때 map 함수를 사용, jsx에서 props를 통해 값 전달, key는 표현되지 않지만 필수 props
+              movies.map((movie) => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))
+            }
+          </div>
+        )}
+      </section>
     );
   }
 }
